cab8d1ce535b8443330bfde12b650593af3fc6eb
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2021-07-06 18:11:33 -0300

    [API] Create Store tests and add contrains

--- api/db/migrate/20210706210451_create_stores.rb
+++ api/db/migrate/20210706210451_create_stores.rb
@@ -1,9 +1,7 @@
 class CreateStores < ActiveRecord::Migration[6.1]
   def change
     create_table :stores do |t|
-      t.string :name
-
-      t.timestamps
+      t.string :name, null: false
     end
   end
 end
--- api/test/models/store_test.rb
+++ api/test/models/store_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class StoreTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "when save store then ok" do
+    store = Store.new
+    store.name = "Mock Store"
+    assert store.save
+    assert_not_nil(store.id)
+  end
+
+  test "when save store without name then fail" do
+    store = Store.new
+    assert_not store.save
+  end
+
+  test "get all" do
+    stores = Store.all
+    assert_equal(2, stores.count)
+  end
 end
