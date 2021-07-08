497fe08a8772f4a66ce30aa6344d7f864656f1ce
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2021-07-08 00:08:46 -0300

    [CLIENT] Update routes to Models/Stores list/details

--- client/src/components/Routes.tsx
+++ client/src/components/Routes.tsx
@@ -1,12 +1,28 @@
 import { Switch, Route } from "react-router-dom";
 import { Error } from "./Error";
 import { InventoryMonitor } from "./InventoryMonitor";
+import { StoreDetails } from "./StoreDetails";
+import { ModelDetails } from "./ModelDetails";
+import { StoreList } from "./StoreList";
+import { ModelList } from "./ModelList";
 
 export function Routes() {
   return (
     <Switch>
       <Route path="/" exact>
         <InventoryMonitor />
+      </Route>
+      <Route path="/stores" exact>
+        <StoreList />
+      </Route>
+      <Route path="/stores/:id" exact>
+        <StoreDetails />
+      </Route>
+      <Route path="/models" exact>
+        <ModelList />
+      </Route>
+      <Route path="/models/:id" exact>
+        <ModelDetails />
       </Route>
       <Route path="*">
         <Error status={404} />
