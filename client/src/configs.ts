f538280078df284e577ff0b370ea91d77353b060
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2021-07-07 21:56:03 -0300

    [CLIENT] Add config file

---
+++ client/src/configs.ts
@@ -0,0 +1,11 @@
+export const APP_NAME = "APP_NAME";
+export const API_URL = "API_URL";
+export const WEBSOCKETS_URL = "WEBSOCKETS_URL";
+export const DATE_FORMAT = "DATE_FORMAT";
+
+export const DEFAULT_VALUES: { [key: string]: string } = {
+  APP_NAME: "Shoes store",
+  API_URL: "http://127.0.0.1:3005/api/v1",
+  WEBSOCKETS_URL: "ws://127.0.0.1:3005/cable",
+  DATE_FORMAT: "DD-MM-YYYY",
+};
